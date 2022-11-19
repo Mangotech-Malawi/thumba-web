7fa0a4485e335d193c251ec570a11d631c1f308c
Senzenjani <smanjawira@gmail.com>
2022-11-19 22:44:27 +0200

    Added service logic for grade and score settings

---
+++ js/services/settings.js
@@ -0,0 +1,160 @@
+import { apiClient } from "./api-client.js";
+
+export function addScore(params) {
+  return apiClient(
+    "/api/v1/score/new",
+    "POST",
+    "json",
+    false,
+    false,
+    params
+  );
+}
+
+export function editScore(params) {
+    return apiClient(
+      "/api/v1/score/edit",
+      "POST",
+      "json",
+      false,
+      false,
+      params
+    );
+}
+
+export function fetchScores() {
+  let data = apiClient("/api/v1/scores", "GET", "json", false, false, {});
+
+  loadScores(data);
+}
+
+function loadScores(dataset) {
+  $("#scoresTable").DataTable({
+    destroy: true,
+    responsive: true,
+    searching: true,
+    ordering: true,
+    lengthChange: true,
+    autoWidth: false,
+    info: true,
+    data: dataset,
+    columns: [
+      { data: "id" },
+      { data: "code" },
+      { data: "name" },
+      { data: "description" },
+      { data: "score" },
+      { data: null },
+      { data: null },
+    ],
+    columnDefs: [
+      {
+        render: getScoreUpdateBtn,
+        data: null,
+        targets: [5],
+      },
+      {
+        render: getScoreDelBtn,
+        data: null,
+        targets: [6],
+      },
+    ],
+  });
+}
+
+export function getScoreUpdateBtn() {
+  let dataFields = `data-score-id = "${data.id}"
+    data-action-type = "edit"`;
+
+  return getButton(dataFields, "analysis-score", "default", "fas fa-edit");
+}
+
+export function getScoreDeleteBtn() {
+  let dataFields = `data-score-del-id = "${data.id}"
+    data-action-type = "edit"`;
+
+  return getButton(dataFields, "analysis-score", "default", "fas fa-edit");
+}
+
+export function addGrade(params) {
+    return apiClient(
+      "/api/v1/grade/new",
+      "POST",
+      "json",
+      false,
+      false,
+      params
+    );
+  }
+  
+  export function editGrade(params) {
+      return apiClient(
+        "/api/v1/grade/edit",
+        "POST",
+        "json",
+        false,
+        false,
+        params
+      );
+  }
+  
+  export function fetchGrades() {
+    let data = apiClient("/api/v1/grades", "GET", "json", false, false, {});
+  
+    loadGrades(data);
+  }
+  
+
+
+function loadGrades(dataset) {
+    $("#gradesTable").DataTable({
+      destroy: true,
+      responsive: true,
+      searching: true,
+      ordering: true,
+      lengthChange: true,
+      autoWidth: false,
+      info: true,
+      data: dataset,
+      columns: [
+        { data: "id" },
+        { data: "name" },
+        { data: "maximum" },
+        { data: "minimum" },
+        { data: null },
+        { data: null },
+      ],
+      columnDefs: [
+        {
+          render: getGradeUpdateBtn,
+          data: null,
+          targets: [4],
+        },
+        {
+          render: getGradeDelBtn,
+          data: null,
+          targets: [5],
+        },
+      ],
+    });
+  }
+  
+  export function getGradeUpdateBtn() {
+    let dataFields = `data-grade-id = "${data.id}"
+      data-action-type = "edit"`;
+  
+    return getButton(dataFields, "analysis-grade", "default", "fas fa-edit");
+  }
+  
+  export function getGradeDelBtn() {
+    let dataFields = `data-grade-del-id = "${data.id}"
+      data-action-type = "edit"`;
+  
+    return getButton(dataFields, "analysis-grade", "default", "fas fa-edit");
+  }
+
+function getButton(dataFields, modal, color, icon) {
+    return `<button type='button' class="btn btn-block btn-${color}" data-toggle="modal" 
+              data-target="#modal-${modal}" ${dataFields} ><i class="${icon}" aria-hidden="true"></i></button>`;
+}
+  
