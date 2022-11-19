bb5bc43e8bf08b320b4459b924ca7d79f3647548
Senzenjani <smanjawira@gmail.com>
2022-11-19 22:42:56 +0200

    Added logic for registering and editing grades and scores settings

---
+++ js/actions/settings.js
@@ -0,0 +1,161 @@
+import * as settings from "../services/settings.js";
+
+const scoresModal = "#modal-scores";
+const gradesModal = "#modal-grades";
+
+$(function () {
+  $(document).on("show.bs.modal", scoresModal, function (e) {
+    let opener = e.relatedTarget;
+
+    if ($(opener).attr("data-action-type") == "edit") {
+      $.each(opener.dataset, function (key, value) {
+        $(scoresModal).find(`[id = '${key}']`).val(value);
+      });
+    } else {
+      $(scoresModal).find(`[id = 'scoresModalTitle']`).text("Add Score");
+    }
+  });
+
+  $(document).on("click", "#saveScoreBtn", function () {
+    console.log("Am here");
+    if ($("#scoreModalTitle").text() === "Add Score") {
+      notification(
+        settings.addScore(scoreParams()).created,
+        "center",
+        "success",
+        "score",
+        "Add Analysis Score",
+        "Analyisis score has been added successfully",
+        true,
+        3000
+      );
+    } else if ($("#scoreModalTitle").text() === "Edit Score") {
+      notification(
+        seetings.editScore(scoreParams()).updated,
+        "center",
+        "success",
+        "score",
+        "Edit Analysis Score",
+        "Analysis score has been updated successfully",
+        true,
+        3000
+      );
+    }
+  });
+
+
+
+
+
+  $(document).on("show.bs.modal", gradesModal, function (e) { 
+    let opener = e.relatedTarget;
+
+    if ($(opener).attr("data-action-type") == "edit") {
+      $.each(opener.dataset, function (key, value) {
+        $(gradesModal).find(`[id = '${key}']`).val(value);
+      });
+    } else {
+      $(gradesModal).find(`[id = 'gradesModalTitle']`).text("Add Grade");
+    }
+  });
+
+
+
+  $(document).on("click", "#saveGradeBtn", function () {
+    if ($("#gradeModalTitle").text() === "Add Grade") {
+      notification(
+        settings.editGrade(gradeParams()).created,
+        "center",
+        "success",
+        "grade",
+        "Add Analysis Grade",
+        "Analyisis grade has been added successfully",
+        true,
+        3000
+      );
+    } else if ($("#gradeModalTitle").text() === "Edit Score") {
+      notification(
+        settings.editGrade(gradeParams()).updated,
+        "center",
+        "success",
+        "grade",
+        "Edit Analysis Grade",
+        "Analysis grade has been updated successfully",
+        true,
+        3000
+      );
+    }
+  });
+
+
+
+
+});
+
+function scoreParams() {
+  let code = $("#code").val();
+  let name = $("#name").val();
+  let description = $("#description").val();
+  let score = $("#score").val();
+
+  let params = {
+    code: code,
+    name: name,
+    description: description,
+    score: score,
+  };
+
+  return params;
+}
+
+
+function gradeParams() {
+    let name = $("#name").val();
+    let minimum = $("#minimum").val();
+    let maximum = $("#maximum").val();
+  
+    let params = {
+      name: name,
+      minimum: minimum,
+      maximum: maximum,
+    };
+  
+    return params;
+}
+  
+
+function notification(
+    isDone,
+    position,
+    icon,
+    recordType,
+    title,
+    text,
+    showConfirmButton,
+    timer
+  ) {
+    if (isDone)
+      $.when(
+        Swal.fire({
+          position: position,
+          icon: icon,
+          title: title,
+          text: text,
+          showConfirmButton: showConfirmButton,
+          timer: timer,
+        })
+      ).done(function () {
+        switch (recordType) {
+          case "score":
+            $.when(loans.fetchScores()).done(function () {
+              $(scoreModal).modal("hide");
+            });
+            break;
+          case "grade":
+            $.when(settings.fetchGrades()).done(function () {
+                $(gradeModal).modal("hide");
+            });
+            break;
+        }
+      });
+  }
