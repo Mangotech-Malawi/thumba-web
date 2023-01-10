5f9780a5db29d0e0269e7149ea4aa19ceac9d971
Senzenjani <smanjawira@gmail.com>
2023-01-10 15:06:39 +0200

    Standardized chart gauges to one type

--- js/services/chartsOptions/risk_results.js
+++ js/services/chartsOptions/risk_results.js
@@ -1,46 +1,81 @@
-export const riskResultOptions = {
-    series: [],
+export const riskResultOptions =  {
+    series: [75],
     chart: {
-    height: 350,
-    type: 'radialBar',
-    offsetY: -10
-  },
-  plotOptions: {
-    radialBar: {
-      startAngle: -135,
-      endAngle: 135,
-      dataLabels: {
-        name: {
-          fontSize: '20px',
-          color: undefined,
-          offsetY: 120
+      height: 350,
+      type: "radialBar",
+      toolbar: {
+        show: true,
+      },
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#fff",
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: "front",
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24,
+          },
         },
-        value: {
-          offsetY: 76,
-          fontSize: '32px',
-          color: undefined,
-          formatter: function (val) {
-            return val + "%";
-          }
-        }
-      }
-    }
-  },
-  fill: {
-    type: 'gradient',
-    gradient: {
-        shade: 'dark',
-        shadeIntensity: 0.15,
-        inverseColors: false,
+        track: {
+          background: "#fff",
+          strokeWidth: "67%",
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35,
+          },
+        },
+  
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: "#888",
+            fontSize: "17px",
+          },
+          value: {
+            formatter: function (val) {
+              return parseInt(val);
+            },
+            color: "#111",
+            fontSize: "36px",
+            show: true,
+          },
+        },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "horizontal",
+        shadeIntensity: 0.5,
+        gradientToColors: ["#00FF00"],
+        inverseColors: true,
         opacityFrom: 1,
         opacityTo: 1,
-        stops: [0, 50, 65, 91]
+        stops: [0, 100],
+      },
     },
-  },
-  stroke: {
-    dashArray: 8
-  },
-  labels: ['Loan Risk'],
-  };
+    stroke: {
+      lineCap: "round",
+    },
+    labels: ["Percent"],
+  };;
 
   export default riskResultOptions;
