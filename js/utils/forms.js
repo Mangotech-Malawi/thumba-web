093cbb3ba6f6ad8d54f6d4cee18ca0057bc2027a
Senzenjani <smanjawira@gmail.com>
2023-06-18 19:40:39 +0200

    Added more validations

--- js/utils/forms.js
+++ js/utils/forms.js
@@ -80,6 +80,84 @@ function validateTraditionalAuthority(traditionalAuthority) {
     return true; // Traditional Authority is valid
 }
 
+
+function validateVillage(village) {
+    var regex = /^[A-Za-z0-9\s-]{1,100}$/;
+  
+    if (!regex.test(village)) {
+      return false; // Village name contains invalid characters or exceeds the maximum length
+    }
+  
+    return true; // Village name is valid
+}
+
+
+function validateAlphanumeric(identifier) {
+    var regex = /^[a-zA-Z0-9]+$/;
+  
+    if (!regex.test(identifier)) {
+      return false; // Identifier contains invalid characters
+    }
+  
+    return true; // Identifier is valid
+}
+
+function validateDescription(description) {
+    // Remove leading/trailing whitespace and line breaks
+    description = description.trim();
+  
+    // Count the number of words in the description
+    var wordCount = description.split(/\s+/).length;
+  
+    if (wordCount > 300) {
+      return false; // Description exceeds the maximum word limit
+    }
+  
+    return true; // Description is valid
+}
+
+function validateStartDate(startDate) {
+    // Get the current date
+    var today = new Date();
+    
+    // Set the time to midnight (00:00:00) to ignore the time component
+    today.setHours(0, 0, 0, 0);
+    
+    // Compare the start date with today's date
+    if (startDate.getTime() > today.getTime()) {
+      return false; // Start date is greater than today
+    }
+  
+    return true; // Start date is valid
+}
+
+
+function validateAlphabeticStringWithSpace(input) {
+    var regex = /^[A-Za-z\s]{1,50}$/;
+  
+    if (!regex.test(input)) {
+      return false; // String does not match the pattern
+    }
+  
+    return true; // String is valid
+}
+
+
+function validateBackDate(input) {
+  // Convert input to Date object
+  var inputDate = new Date(input);
+
+  // Get today's date
+  var today = new Date();
+
+  if (inputDate > today) {
+    return false; // Date is greater than today
+  }
+
+  return true; // Date is less than or equal to today
+}
+  
+
 function  getButton(dataFields, modal, color, icon) {
     return `<button type='button' class="btn btn-${color}" data-toggle="modal" 
             data-target="#modal-${modal}" ${dataFields} ><i class="${icon}" aria-hidden="true"></i></button>`;
