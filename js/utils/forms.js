374d6ae8498d7c09c20d20280b6f71f56500a0e6
Senzenjani <smanjawira@gmail.com>
2023-07-05 15:05:43 +0200

    Added job forms

--- js/utils/forms.js
+++ js/utils/forms.js
@@ -2,7 +2,7 @@ import { validate } from "../utils/validations.js"
 
 let formElements = []
 
-export function validClientFormData(){
+export function validClientFormData() {
   formElements = []
   let validClientData = false;
   pushFormElements("nationalId", "#nationalId", true, "National ID");
@@ -13,15 +13,37 @@ export function validClientFormData(){
   pushFormElements("village", "#homeVillage", true, "Home Village");
   pushFormElements("ta", "#currentTa", true, "Current TA");
   pushFormElements("village", "#currentVillage", true, "Current Village");
- 
-  $.when(validate(formElements)).done( function (value){
-       validClientData = value
+
+  $.when(validate(formElements)).done(function (value) {
+    validClientData = value
   });
 
   return validClientData;
 }
 
-function pushFormElements(type, id, isFilled, name){
+export function validClientJobFormData() {
+  formElements = []
+  let validData = false;
+
+  pushFormElements("description", "#title", true, "Job Title");
+  pushFormElements("description", "#department", true, "Department");
+  pushFormElements("description", "#employerName", true, "Employer Name");
+  pushFormElements("startDate", "#dateStarted", true, "Date Started");
+  pushFormElements("futureDate", "#contractDue", true, "Contract Due");
+  pushFormElements("moneyAmount", "#netSalary", true, "Net Salary");
+  pushFormElements("moneyAmount", "#grossSalary", true, "Gross Salary");
+  pushFormElements("description", "#postalAddress", true, "Postal Address");
+  pushFormElements("email", "#emailAddress", true, "Employer Email Address");
+  pushFormElements("phoneNumber", "#phoneNumber", true, "Employer Phone Number");;
+
+  $.when(validate(formElements)).done(function (value) {
+    validData = value
+  });
+
+  return validData;
+}
+
+function pushFormElements(type, id, isFilled, name) {
   formElements.push({
     type: type,
     id: id,
