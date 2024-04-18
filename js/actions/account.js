6bc41a1726e23c336c25d8bdab0acf98a808504b
Senzenjani <smanjawira@gmail.com>
2024-04-18 15:19:33 +0200

    Added logic to register account

--- js/actions/account.js
+++ js/actions/account.js
@@ -1,19 +1,20 @@
 import * as account from "../services/account.js";
 import * as form from "../utils/forms.js"
+import { toastNote } from "../utils/utils.js"
 
 $(document).ready(function () {
-    $(document).on("click","#registerBtn", function (e){
+    $(document).on("click", "#registerBtn", function (e) {
         e.preventDefault();
 
-        //Validation to be added for accounts password an other details
-        if (password !== "" && conPassword !== null){
-
+        if(account.register(registerAccountParams()).created){
+            window.location("index.html");
         }
+
     });
 
-    $(document).on('blur', '#password', function (e){
-        let password   = $("#password").val();
-        
+    $(document).on('blur', '#password', function (e) {
+        let password = $("#password").val();
+
         $("#wrongPassword").text("");
 
         if (password !== "" && password !== null) {
@@ -31,9 +32,9 @@ $(document).ready(function () {
 
     });
 
-    $(document).on('blur', '#confirmPassword', function(e){
+    $(document).on('blur', '#confirmPassword', function (e) {
         let confirmPassword = $("#confirmPassword").val();
-        let password   = $("#password").val();
+        let password = $("#password").val();
 
         $("#wrongPasswordMatch").text("");
 
@@ -41,35 +42,76 @@ $(document).ready(function () {
             if (!(password === confirmPassword)) {
                 $("#wrongPasswordMatch").text(`Does not match with new password`);
                 $("#registerBtn").attr('disabled', true);
-            }else{
+            } else {
                 $("#registerBtn").attr('disabled', false);
             }
-        }else {
+        } else {
             $("#registerBtn").attr('disabled', true);
-        }        
+        }
 
     });
 });
 
 
-function registerAccountParams(){
+function registerAccountParams() {
     let name = $("#name").val();
     let address = $("#address").val();
     let email = $("#email").val();
     let phoneNumber = $("#phoneNumber").val();
-    let password   = $("#password").val();
-    let conPassword = $("#confPassword").val();
+    let username   = $("#username").val();
+    let userEmail = $("#userEmail").val();
+    let nationalId = $("#nationalId").val();
+    let firstname = $("#firstname").val();
+    let lastname = $("#lastname").val();
+    let password = $("#password").val();
 
     let params = {
-        name: name, 
+        name: name,
         address: address,
-        email: email, 
-        phoneNumber: phoneNumber,
-        password: password
+        email: email,
+        phone_number: phoneNumber,
+        password: password,
+        username: username,
+        user_email: userEmail, 
+        firstname: firstname,
+        lastname: lastname,
+        national_id: nationalId
     }
 
     return params
 }
+
+function notification(
+    isDone,
+    position,
+    icon,
+    recordType,
+    title,
+    text,
+    showConfirmButton,
+    timer
+) {
+    if (isDone)
+        $.when(
+            Swal.fire({
+                position: position,
+                icon: icon,
+                title: title,
+                text: text,
+                showConfirmButton: showConfirmButton,
+                timer: timer,
+            })
+        ).done(function () {
+            switch (recordType) {
+                case "accounts":
+                    $.when(investment.fetchInvestimentPackages()).done(function () {
+                        $(investmentPackageModal).modal("hide");
+                    });
+                    break;
+            }
+        });
+}
+
 
 
 function validatePassword(password) {
