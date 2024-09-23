05f989b04ad42ebb739ce054d24e131402617881
Senzenjani <smanjawira@gmail.com>
2024-09-23 05:35:07 +0200

    Controlling visibility of password per user request when loginng in

--- js/actions/login.js
+++ js/actions/login.js
@@ -3,21 +3,37 @@ import * as user from "../services/users.js";
 
 $(document).ready(function () {
 
-    $('#loginForm').on('submit', function(e){
-
-        e.preventDefault();
-
-        let username = $('username').val();
-        let password = $('password').val();
-        var formData = $(this).serialize();
+    $('#loginBtn').on('click', function(e){
+        let username = $('#email').val();
+        let password = $('#password').val();
         
-        user.login(formData );
+        user.login({username: username, password: password} );
+    });
+
+    $(document).on('click', '#passwordVisibility', function(e){
+        if($("#passwordVisibility").hasClass("fa-eye"))
+            passwordVisibility("text","fa-eye", "fa-eye-slash");
+        else
+            passwordVisibility("password","fa-eye-slash", "fa-eye");
+    
     });
 
     $('#homeBtn').on('click', function(e){
         window.location = "index.html";
     });
 
-    
+    $(document).on('keyup', '#email', function (e) {
+        $("#invalidCredentials").text("");
+    });
+
+    $(document).on('keyup', '#password', function (e) {
+        $("#invalidCredentials").text("");
+    });
 
 });
+
+function passwordVisibility(attr, oldIconClass, newIconClass){
+    $("#password").attr("type", attr);
+    $("#passwordVisibility").removeClass(oldIconClass).addClass(newIconClass);
+
+}
