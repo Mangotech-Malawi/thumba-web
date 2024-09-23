d6e40ec199088e9c000cb4fe634370a13085278e
Senzenjani <smanjawira@gmail.com>
2024-09-23 20:47:04 +0200

    Fixed API client issue

--- js/services/api-client.js
+++ js/services/api-client.js
@@ -1,7 +1,7 @@
 
 import * as conn_data from "./config.js";
 
-let config = nil
+let config = null;
 
 
 let token = sessionStorage.getItem("token");
@@ -10,31 +10,33 @@ export function apiClient(path, type, dataType, async, cache, data) {
     let result = null
 
     $.when(conn_data.getConfigs()).done(function (data){
-        config = data
-    });
+        config = data;
 
-    $.ajax({
-        url: `${config.apiProtocol}://${config.apiURL}:${config.apiPort}${path}`,
-        type: type,
-        dataType: dataType,
-        async: async,
-        cache: cache,
-        headers: {
-            'Authorization': 'Bearer ' + token
-        },
-        data: data,
-        success: function (res) {
-            result = res
-        },
-        error: function(res){
-            if(res.status === 401){
-               sessionStorage.clear();
-               localStorage.clear();
+        $.ajax({
+            url: `${config.apiProtocol}://${config.apiURL}:${config.apiPort}${path}`,
+            type: type,
+            dataType: dataType,
+            async: async,
+            cache: cache,
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+            data: data,
+            success: function (res) {
+                result = res
+            },
+            error: function(res){
+                if(res.status === 401){
+                   sessionStorage.clear();
+                   localStorage.clear();
+                } 
             } 
-        } 
-    }).fail(function (jqXHR, testStatus, errorThrown) {
-
+        }).fail(function (jqXHR, testStatus, errorThrown) {
+    
+        });
     });
+
+    
 
     return result;
 }
