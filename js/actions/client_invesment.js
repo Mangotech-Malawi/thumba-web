f2ca010034d5448c2639e96cd2e36c2b5c9ca2db
Senzenjani <smanjawira@gmail.com>
2024-11-18 22:24:27 +0200

    Refactor

--- js/actions/client_invesment.js
+++ js/actions/client_invesment.js
@@ -1,37 +1,58 @@
 import * as client from "../services/clients.js";
 import * as form from "../utils/forms.js";
 import * as contentLoader from "../actions/contentLoader.js";
+import * as investment from "../services/investments.js";
 
 let currentDataset = null;
 localStorage;
 
 $(function () {
 
-    if (localStorage.getItem("clientDataSet") != null) {
-        currentDataset = JSON.parse(localStorage.getItem("clientDataSet"));
 
-        $(document).on("click", "#btnInvestments", function (e) {
-            $.when(contentLoader.loadIndividualRecordView("views/clients/investments.html", "client_investments")).done(
-                function () {
-                    $("#recordName").text(
-                        `${currentDataset.recordFirstname} ${currentDataset.recordLastname} Assets`
-                    );
-
-                    client.fetchClientInvesments({
-                        client_id: currentDataset.recordId,
-                    });
-                }
-            );
-        });
-    }
+    $(document).on("click", "#btnInvestments", function (e) {
+        clientInvestmentsView();
+    });
 
     $(document).on("click", "#addSub", function (e) {
         $.when(contentLoader.loadIndividualRecordView("views/forms/investment.html", "add_investment")).done(
             function () {
-                $("#recordName").text(
-                    `${currentDataset.recordFirstname} ${currentDataset.recordLastname} Demographics`
-                );
+                let investmentPackagesArray = [];
+                let investmentPackages = investment.fetchInvestimentPackages("load-none");
+
+                if (investmentPackages !== null) {
+                    investmentPackages.forEach(function (investment_package, index) {
+                        investmentPackagesArray.push(
+                            '<option value ="',
+                            investment_package.id,
+                            '">',
+                            `${investment_package.package_name} | ${investment_package.interest_rate}%`,
+                            "</option>"
+                        )
+                    });
+
+                    $("#investmentPackageId").html(investmentPackagesArray.join(""));
+                }
             }
         );
     });
 });
+
+function clientInvestmentsView() {
+
+    if (localStorage.getItem("clientDataSet") != null) {
+        currentDataset = JSON.parse(localStorage.getItem("clientDataSet"));
+
+        $.when(contentLoader.loadIndividualRecordView("views/clients/investments.html", "client_investments")).done(
+            function () {
+                $("#recordName").text(
+                    `${currentDataset.recordFirstname} ${currentDataset.recordLastname} Assets`
+                );
+
+                client.fetchClientInvesments({
+                    client_id: currentDataset.recordId,
+                });
+            }
+        );
+    }
+
+}
