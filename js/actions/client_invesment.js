826d1875c25dd7ace6d87c39cbdfe018a179b8a4
Senzenjani <smanjawira@gmail.com>
2024-11-19 01:19:14 +0200

    Saving investment subscription

--- js/actions/client_invesment.js
+++ js/actions/client_invesment.js
@@ -19,23 +19,100 @@ $(function () {
                 let investmentPackagesArray = [];
                 let investmentPackages = investment.fetchInvestimentPackages("load-none");
 
+
                 if (investmentPackages !== null) {
-                    investmentPackages.forEach(function (investment_package, index) {
+                    let investmentPackagesArray = [];
+                    investmentPackages.forEach(function (investment_package) {
                         investmentPackagesArray.push(
-                            '<option value ="',
-                            investment_package.id,
+                            '<option value="',
+                            encodeURIComponent(JSON.stringify(investment_package)),
                             '">',
                             `${investment_package.package_name} | ${investment_package.interest_rate}%`,
-                            "</option>"
-                        )
+                            '</option>'
+                        );
                     });
 
                     $("#investmentPackageId").html(investmentPackagesArray.join(""));
                 }
+
             }
         );
     });
+
+    $(document).on("change","#investmentPackageId", function () {
+        const selectedPackage = $(this).val();
+        if (selectedPackage) {
+            const investmentPackageObject = JSON.parse(decodeURIComponent(selectedPackage));
+
+            // Update the labels
+            $("#minAmount").text(investmentPackageObject.min_amount);
+            $("#maxAmount").text(investmentPackageObject.max_amount);
+            $("#interestRate").text(`${investmentPackageObject.interest_rate}%`);
+            $("#interestFrequency").text(investmentPackageObject.interest_frequency);
+            $("#duration").text(`${investmentPackageObject.duration} ${investmentPackageObject.duration_type}`);
+            $("#currency").text(investmentPackageObject.currency);
+            $("#compoundingFrequency").text(investmentPackageObject.compounding_frequency);
+
+            // Show the details section if hidden
+            $("#investmentPackageDetails").show();
+        } else {
+            // Hide the details section if no package is selected
+            $("#investmentPackageDetails").hide();
+        }
+    });
+
+
+
+    $(document).on("click", "#investmentSubBtn", function (e) {
+        if (form.validateInvestmentForm()) {
+            if ($("#formTitle").text().trim() === "Add Investment Subscription") {
+                notification(
+                    investment.addInvestment(getInvestmentParams()).created,
+                    "center",
+                    "success",
+                    "investment",
+                    "Add Investment",
+                    "Investment has been added successfully",
+                    true,
+                    3000
+                );
+            } else if ($("#formTitle").text() === "Edit Investment Subscription") {
+                notification(
+                    investment.editInvestment(getInvestmentParams()).updated,
+                    "center",
+                    "success",
+                    "investment",
+                    "Edit Investment",
+                    "Investment has been edited successfully",
+                    true,
+                    3000
+                );
+            }
+        }
+    });
+
 });
+
+function getInvestmentParams() {
+    
+    let investment_id = $("#investmentId").val();
+  
+    let investment_package_id =   JSON.parse(decodeURIComponent($("#investmentPackageId").val())).id
+    let amount = $("#amount").val();
+    let description = $("#description").val();
+    let investment_date = $("#investmentDate").val();
+
+    let params = {
+        id: investment_id,
+        client_id: currentDataset.recordId,
+        investiment_package_id: investment_package_id,
+        amount: amount,
+        description: description,
+        investment_date: investment_date
+    }
+
+    return params;
+}
 
 function clientInvestmentsView() {
 
@@ -48,11 +125,40 @@ function clientInvestmentsView() {
                     `${currentDataset.recordFirstname} ${currentDataset.recordLastname} Assets`
                 );
 
-                client.fetchClientInvesments({
-                    client_id: currentDataset.recordId,
-                });
+               
             }
         );
     }
 
 }
+
+
+function notification(
+    isDone,
+    position,
+    icon,
+    recordType,
+    title,
+    text,
+    showConfirmButton,
+    timer
+) {
+    if (isDone)
+        $.when(
+            Swal.fire({
+                position: position,
+                icon: icon,
+                title: title,
+                text: text,
+                showConfirmButton: showConfirmButton,
+                timer: timer,
+            })
+        ).done(function () {
+            switch (recordType) {
+                case "investment":
+                    clientInvestmentsView();
+                    break;
+            }
+        });
+}
+
