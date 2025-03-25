37f6f3d70d50a110fc11e6a5f92624156c136b2a
Senzenjani <smanjawira@gmail.com>
2025-03-25 10:28:26 +0200

    Updated links privileges

--- js/app-views/links.js
+++ js/app-views/links.js
@@ -1,38 +1,69 @@
 export const links = [
+
+    {
+        privilege: "system_reports",
+        link: "views/links/system_reports.html",
+        position: 1,
+    },
+    {
+        privilege: "admin_reports",
+        link: "views/links/admin_reports.html",
+        position: 2,
+    },
+    {
+        privilege: "branch_reports",
+        link: "views/links/branch_reports.html",
+        position: 3,
+    },
+    {
+        privilege: "owner_reports",
+        link: "views/links/owner_reports.html",
+        position: 4,
+    },
+    {
+        privilege: "loan_officer_reports",
+        link: "views/links/loan_officer_reports.html",
+        position: 5,
+    },
+    {
+        privilege: "client_reports",
+        link: "views/links/client_reports.html",
+        position: 6,
+    },
     {
         privilege: "manages_clients",
         link: "views/links/clients.html",
-        position: 1, 
+        position: 7,
     },
     {
         privilege: "manages_applications",
         link: "views/links/loans.html",
-        position: 2, 
+        position: 8,
     },
     {
         privilege: "manages_products",
         link: "views/links/products.html",
-        position: 3, 
+        position: 9,
     },
     {
         privilege: "manages_tenants",
         link: "views/links/superuser.html",
-        position: 4, 
+        position: 10,
     },
     {
         privilege: "manages_finances",
         link: "views/links/finances.html",
-        position: 5, 
+        position: 11,
     },
     {
         privilege: "manages_users",
         link: "views/links/users.html",
-        position: 6, 
+        position: 12,
     },
-
     {
         privilege: "manages_settings",
         link: "views/links/settings.html",
-        position: 7,
-    }
+        position: 13,
+    },
+
 ]
