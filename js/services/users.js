1f5988c27cb53017208dfa3ce95db35794f496c2
Senzenjani <smanjawira@gmail.com>
2025-05-13 20:02:55 +0200

    Clearing session before startng a new one

--- js/services/users.js
+++ js/services/users.js
@@ -40,8 +40,13 @@ export function login(params) {
 
       $.when(saveSessionDetails(data)).done(
         function () {
-          if (sessionStorage.getItem("token") && !sessionStorage.getItem("selected_branch_id") && 
-              sessionStorage.getItem("branch_user_roles") != "" ) {
+
+          const token = sessionStorage.getItem("token")
+          const branch_user_roles = JSON.parse(sessionStorage.getItem("branch_user_roles"));
+          const branch_role_id = sessionStorage.getItem("selected_branch_id");
+        
+          if (token && branch_role_id && 
+              branch_user_roles.length > 0  ) {
               window.location = "branch_selection.html";
             
             } else  {
