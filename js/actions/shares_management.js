c1bda7df9044460cb6f2277fa66a25a233269495
Senzenjani <smanjawira@gmail.com>
2025-06-22 17:27:38 +0200

    Added logic for saving sharholder and capital contribution

--- js/actions/shares_management.js
+++ js/actions/shares_management.js
@@ -1,6 +1,10 @@
 import * as share_management from "../services/share_management.js";
+import * as form from "../utils/forms.js";
 import * as loadContent from "../actions/contentLoader.js";
 import { loadIdentifierTypes } from "../services/users.js";
+
+const capitalContributionModal = "#modal-capital-contribution";
+let capitalContributionModalTitle = "#capitalContributionModalTitle";
 
 $(function () {
     $(document).on("click", "#btnShareholderBtn", function () {
@@ -11,4 +15,190 @@ $(function () {
             }
         );
     });
+
+    $(document).on("click", "#saveShareholderBtn", function () {
+        if (form.validClientFormData()) {
+            if ($("#cardTitle").text().trim() === "Add Shareholder") {
+                notification(
+                    share_management.addShareholder(shareholderParams()).created,
+                    "center",
+                    "success",
+                    "registration",
+                    "Add Shareholder",
+                    "Shareholder has been added successfully",
+                    true,
+                    3000
+                );
+            } else if ($("#cardTitle").text().trim() === "Edit Shareholder") {
+                notification(
+                    share_management.updateShareholder(shareholderParams()).uppdated,
+                    "center",
+                    "success",
+                    "registration",
+                    "Edit Shareholder",
+                    "Shareholder has been updated successfully",
+                    true,
+                    3000
+                );
+            }
+        }
+    });
+
+    $(document).on("show.bs.modal", capitalContributionModal, function (e) {
+        const opener = e.relatedTarget;
+
+        $(capitalContributionModal).find(`[id= 'shareholderId']`)
+            .val($(opener).attr("data-shareholder-id"));
+
+        if ($(opener).attr("data-action-type") === "edit") {
+            $(capitalContributionModalTitle).text("Edit Capital Contribution");
+        } else {
+            $(capitalContributionModalTitle).text("Add Capital Contribution");
+        }
+
+        populateShareClasses(share_management.fetchShareClasses());
+    });
+
+    $(document).on("click", "#saveCapitalContributionBtn", function () {
+        if (form.validCapitalContributionFormData()) {
+            if ($(capitalContributionModalTitle).text().trim() === "Add Capital Contribution") {
+                notification(
+                    share_management.addCapitalContribution(capitalContributionParams()).created,
+                    "center",
+                    "success",
+                    "capital_contribution",
+                    "Add Capital Contribution",
+                    "Capital Contribution has been added successfully",
+                    true,
+                    3000
+                );
+            } else if ($(capitalContributionModalTitle).text().trim() === "Edit Capital Contribution") {
+                notification(
+                    share_management.editCapitalContribution(capitalContributionParams()).created,
+                    "center",
+                    "success",
+                    "capital_contribution",
+                    "Edit Capital Contribution",
+                    "Capital Contribution has been updated successfully",
+                    true,
+                    3000
+                );
+            }
+        }
+    });
+
+
+
+
 })
+
+// Shareholder params
+function shareholderParams() {
+    let shareholder_id = $("#shareholderId").val();
+    let identifier = $("#identifier").val();
+    let identifierTypeId = $("#identifierType").val();
+    let firstname = $("#firstname").val();
+    let lastname = $("#lastname").val();
+    const email_address = $("#emailAddress").val();
+    const phone_number = $("#phoneNumber").val();
+    let gender = $("#gender option:selected").val();
+    let date_of_birth = $("#dateOfBirth").val();
+    let home_district = $("#homeDistrict option:selected").val();
+    let home_ta = $("#homeTa").val();
+    let home_village = $("#homeVillage").val();
+    let current_district = $("#currentDistrict option:selected").val();
+    let current_ta = $("#currentTa").val();
+    let current_village = $("#currentVillage").val();
+
+    let params = {
+        shareholder_id: shareholder_id,
+        identifier: identifier,
+        identifier_type_id: identifierTypeId,
+        firstname: firstname,
+        lastname: lastname,
+        email_address: email_address,
+        phone_number: phone_number,
+        gender: gender,
+        date_of_birth: date_of_birth,
+        home_district: home_district,
+        home_ta: home_ta,
+        home_village: home_village,
+        current_district: current_district,
+        current_ta: current_ta,
+        current_village: current_village,
+    };
+
+    return params;
+}
+
+// Capital contribution params
+function capitalContributionParams() {
+    const shareholder_id = $("#shareholderId").val()
+    const share_class_id = $("#shareClassSelector").val();
+    const amount = $("#amount").val();
+    const contributed_date = $("#contributedDate").val();
+
+    const params = {
+        shareholder_id: shareholder_id,
+        share_class_id: share_class_id,
+        amount: amount,
+        contributed_date: contributed_date
+    }
+
+    return params;
+}
+
+function notification(
+    isDone,
+    position,
+    icon,
+    recordType,
+    title,
+    text,
+    showConfirmButton,
+    timer
+) {
+    if (isDone)
+        $.when(
+            Swal.fire({
+                position: position,
+                icon: icon,
+                title: title,
+                text: text,
+                showConfirmButton: showConfirmButton,
+                timer: timer,
+            })
+        ).done(function () {
+            switch (recordType) {
+                case "registration":
+                    $.when(loadContent.loadRecord("views/share_management/shareholders.html", "shareholders")).done(function () {
+                        share_management.fetchShareholders();
+                    });
+                    break;
+                case "capital_contribution":
+                    $.when(loadContent.loadRecord("views/share_management/capital_contributions.html", "capital_contribution")).done(function () {
+                        share_management.fetchCapitalContributions();
+                        $(capitalContributionModal).modal("hide");
+                    });
+                    break;
+            }
+        });
+}
+
+
+function populateShareClasses(shareClasses) {
+
+    let shareClassArray = [];
+
+    shareClasses.forEach(function (share_class, index) {
+        shareClassArray.push(
+            '<option value ="',
+            share_class.id,
+            '">',
+            `${share_class.code} | ${share_class.name}`,
+            "</option>"
+        );
+    });
+
+    $("#shareClassSelector").html(shareClassArray.join(""));
+}
