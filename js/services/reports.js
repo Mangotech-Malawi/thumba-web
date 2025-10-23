7633cca7b705cc2db63f0c9cba7464a2788990dc
Senzenjani <smanjawira@gmail.com>
2025-10-23 10:16:49 +0200

    Added loan report service

--- js/services/reports.js
+++ js/services/reports.js
@@ -1,9 +1,13 @@
 import { apiClient  } from "./api-client.js";
 
 export function users(params) {
-    return apiClient("/api/v1/report/users", "GET", "json", false, false, params);
+  return apiClient("/api/v1/report/users", "GET", "json", false, false, params);
 }
 
 export function admin(params){
-     return apiClient("/api/v1/report/admin", "GET", "json", false, false, params);
+  return apiClient("/api/v1/report/admin", "GET", "json", false, false, params);
+}
+
+export function loan(params){
+  return apiClient("/api/v1/report/loan", "GET", "json", false, false, params);
 }
